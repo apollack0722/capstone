066998c4f7d0dd1109cdacb51abd8321d3ca8ba6
leslieguan <lguan@xula.edu>
2021-03-26 14:01:20 -0500

    main index working

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const { PORT = 3000 } = process.env;
+
+const express = require('express');
+const server = express();
+
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+server.use(express.json());
+server.use(express.urlencoded({extended: true})) //browser comp. 
+
+server.use(morgan("dev"));
+
+server.use(cors());
+
+server.use(bodyParser.json());
+
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
+
+const client = require("./db/client");
+
+
+server.listen(PORT, () => {
+  console.log("The server is up on port", PORT);
+  client.connect();
+});
+
