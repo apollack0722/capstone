b53c042f36bcbabe29c7ce3ff4cfb26c4fc04d37
Aaron Pollack <apollack0722@gmail.com>
2021-03-28 17:54:37 -0500

    base level functionality of routes/db/and FE

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-require("dotenv").config();
-
-const { PORT = 3001 } = process.env;
-
 const express = require('express');
 const server = express();
+const client = require("./backend/db/client");
+require("dotenv").config();
+const { PORT = 3001 } = process.env;
 
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-
 server.use(express.json());
 server.use(express.urlencoded({extended: true})) //browser comp?. 
-
 server.use(morgan("dev"));
-
 server.use(cors());
-
 server.use(bodyParser.json());
 
 const apiRouter = require("./backend/api");
 server.use("/api", apiRouter);
 const userRouter = require("./backend/api");
-server.use("/user", userRouter);
-
-const client = require("./backend/db/client");
-
+server.use("/users", userRouter);
 
 server.listen(PORT, () => {
   console.log("The server is up on port", PORT);
