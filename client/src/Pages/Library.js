fd0a6b9de68053cd6c2d37bfdc9dd26628b94a8e
Aaron Pollack <apollack0722@gmail.com>
2021-04-07 14:10:17 -0500

    library updated with useEffect

--- client/src/Pages/Library.js
+++ client/src/Pages/Library.js
@@ -1,21 +1,28 @@
-import {React, useState} from 'react';
-
+import { useEffect, useState } from 'react';
+//make the posters responsive with class="img-fluid"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Library = () => {
   const [media, setMedia] = useState([])
-    fetch('http://localhost:3001/api/media', {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        })
-        .then(response => response.json())
-        .then(result => {
-            setMedia(result)
-        })
-        .catch(console.error);
+  const getMedia = async() => {await fetch('http://localhost:3001/api/media', {
+    headers: {
+        'Content-Type': 'application/json',
+    },
+  })
+    .then(response => response.json())
+    .then(result => {
 
-
+        setMedia(result)
+        
+    })
+    .catch(console.error);
+  }
+  
+  useEffect(() => {
+    getMedia();
+    
+  }, []);
+  console.log(media)
   return (
     <div>
          {
@@ -28,11 +35,10 @@ const Library = () => {
                     <p>Rental Price: {media.rentalPrice}</p>
                     <p>Purchase Price: {media.buyPrice}</p>
                     <p>{media.rating}</p>
-                    <p>{media.imgUrl}</p>
+                    <img src={media.imgUrl} />
                     </div>
              )
          }
-
     </div>
   )
 }
